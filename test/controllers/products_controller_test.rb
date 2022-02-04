fc28f37a0ffa723e65f8196da6daa65546467d6e
eduardosmatheus <matheus-edu@outlook.com>
2022-02-04 16:46:52 -0300

    Add products to a cart.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -60,4 +60,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+
+  test 'cant delete a product in a cart' do
+    assert_difference "Product.count", 0 do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
+
+
 end
